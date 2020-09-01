b428067ea6e0ccde1b93146d7b7a73c81654609a
achchu93 <achchu.zats@gmail.com>
2020-09-01 15:58:51 +0530

    tweak: faisal code changes

--- templates/new-product.php
+++ templates/new-product.php
@@ -69,8 +69,33 @@ use WeDevs\Dokan\Walkers\TaxonomyDropdown;
                         <a class="dokan-close" data-dismiss="alert">&times;</a>
                         <strong><?php esc_html_e( 'Success!', 'dokan-lite' ); ?></strong>
                         <?php printf( __( 'You have successfully created <a href="%s"><strong>%s</strong></a> product', 'dokan-lite' ), esc_url( dokan_edit_product_url( intval( $get_data['created_product'] ) ) ), get_the_title( intval( $get_data['created_product'] ) ) ); // phpcs:ignore WordPress.XSS.EscapeOutput.OutputNotEscaped ?>
+                    
                     </div>
                 <?php endif ?>
+
+
+                <?php if ( isset( $_GET['message'] ) && $_GET['message'] == 'success') { ?>
+                    <div class="dokan-message">
+                        <button type="button" class="dokan-close" data-dismiss="alert">&times;</button>
+                        <strong><?php esc_html_e( 'Success!', 'dokan-lite' ); ?></strong> <?php esc_html_e( 'The product has been saved successfully.', 'dokan-lite' ); ?>
+
+                        <?php if ( $post->post_status == 'publish' ) { ?>
+                            <a href="<?php echo esc_url( get_permalink( $post_id ) ); ?>" target="_blank"><?php esc_html_e( 'View Product &rarr;', 'dokan-lite' ); ?></a>
+                        <?php } ?>
+                    </div>
+                <?php } ?>
+
+                <?php if ( !empty( $_GET['new'] ) ) { ?>
+                    <div class="dokan-message">
+                        <button type="button" class="dokan-close" data-dismiss="alert">&times;</button>
+                        <strong><?php echo sprintf( 'Your new product added successfully. Product ID is: %d', $post_id ) ?></strong>
+
+                        <?php if ( $post->post_status == 'publish' ) { ?>
+                            <a href="<?php echo esc_url( get_permalink( $post_id ) ); ?>" target="_blank"><?php esc_html_e( 'View Product &rarr;', 'dokan-lite' ); ?></a>
+                        <?php } ?>
+                    </div>
+                <?php } ?>
+
 
                 <?php
 
