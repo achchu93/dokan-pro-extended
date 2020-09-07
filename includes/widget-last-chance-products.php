abe4c6ca6e674a527ea8e4176179a48b4a91f4f9
achchu93 <achchu.zats@gmail.com>
2020-09-07 18:08:15 +0530

    tweak: last chance filter completion

--- includes/widget-last-chance-products.php
+++ includes/widget-last-chance-products.php
@@ -14,8 +14,11 @@ class DPE_Last_Chance_Product_Widget extends WC_Widget {
 				'type'  => 'text',
 				'std'   => 'Last chance products',
 				'label' => 'Title',
-			),
+			)
 		);
+
+		add_action( 'woocommerce_product_query', [ $this, 'last_chance_filter' ], 99, 2 );
+
 		parent::__construct();
 	}
 
@@ -41,8 +44,9 @@ class DPE_Last_Chance_Product_Widget extends WC_Widget {
 		<form method="get" action="<?php echo esc_url( $form_action ); ?>">
 			<div class="last-chance-products-wrapper">
 				<label for="">Last chance products</label>
-				<input type="checkbox" name="last_chance_products" id="last_chance_products" <?php checked( $is_checked, true, true ) ?> >
+				<input type="checkbox" name="last_chance_products" id="last_chance_products" value="1" <?php checked( $is_checked, true, true ) ?> >
 			</div>
+			<button class="button btn" type="submit"  style="width: 100%; margin-top:30px;">Filter</button>
 		</form>
 
 		<?php
@@ -51,7 +55,45 @@ class DPE_Last_Chance_Product_Widget extends WC_Widget {
 	
 	protected function get_current_page_url(){
 
-		return parent::get_current_page_url();
+		$link = parent::get_current_page_url();
+
+		if ( isset( $_GET['last_chance_products'] ) ) {
+			$link = add_query_arg( 'last_chance_products', (bool)$_GET['last_chance_products'] == 'on' ? true : false , $link );
+		}
+
+		return $link;
+	}
+
+
+	public function last_chance_filter( $q, $instance ){
+		
+		if( empty( $_GET['last_chance_products'] ) ) {
+			return;
+		}
+
+		$users = get_users(
+			array(
+				'role__in'   => array( 'seller', 'administrator' ),
+				'number'     => -1,
+				'status'     => 'approved',
+				'fields'  	 => 'ID',
+				'meta_query' => array(
+					'relationship' => 'AND',
+					array(
+						'key' 	  => 'product_pack_enddate',
+						'compare' => 'BETWEEN',
+						'type' 	  => 'DATE',
+						'value'   => [ date('Y-m-d 00:00:00'), date( 'Y-m-d H:i:s', strtotime( "+3 days" ) ) ]
+					)
+				),
+			)
+		);
+
+		if( count( $users ) ) {
+			$q->set( 'author__in', $users );
+		}else{
+			$q->set( 'author__in', array( 0 ) );
+		}
 	}
 
 }
